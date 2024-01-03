b59be5524f44e5fccbbc2d81eb5f02caceb34a63
Quang <lukequang999@gmail.com>
2024-01-03 13:29:59 +0700

    modified:   backend.py 	new file:   templates/AccountManager.html 	modified:   templates/EditContent.html 	modified:   templates/base.html 	modified:   templates/content.html 	modified:   templates/login.html 	modified:   templates/member.html

--- backend.py
+++ backend.py
@@ -15,9 +15,9 @@ password = ''
 try:
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
     cursor = conn.cursor()
-    print('Kết nối database thành công')
+    print('Kết nối database thành công'.encode('utf-8'))
 except Exception as e:
-    print('Kết nối database thất bại:', str(e))
+    print('Kết nối database thất bại:'.encode('utf-8'), str(e))
 
 class User(UserMixin):
     def __init__(self, user_id, username, email, password):
@@ -122,6 +122,18 @@ def logout():
     logout_user()
     flash('Đăng xuất thành công!', 'success')
     return redirect(url_for('index'))
+
+@app.route('/delete_article/<int:article_id>', methods=['DELETE'])
+@login_required
+def delete_article(article_id):
+    try:
+        cursor.execute("DELETE FROM Articles WHERE ID_article = ?", article_id)
+        conn.commit()
+        return "Bài viết đã được xóa thành công"
+    except Exception as e:
+        print("Đã xảy ra lỗi khi xóa bài viết:", e)
+        return "Đã xảy ra lỗi khi xóa bài viết"
+
 @app.route('/create_article', methods=['GET', 'POST'])
 @login_required
 def create_article():
@@ -155,5 +167,82 @@ def create_article():
             flash(f'Lỗi khi thêm bài viết: {str(e)}', 'danger')
 
     return render_template('AddContent.html')
+
+@app.route('/article/<int:ID_article>')
+def display_article(ID_article):
+    cursor.execute('SELECT Title, Content FROM Articles WHERE ID_article = ?', (ID_article,))
+    article = cursor.fetchone()
+
+    if article is None:
+        return 'Article not found'
+    return render_template('content.html', article=article)
+
+app.route('/edit_article/<article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        content = request.form['content']
+        author = current_user.username
+
+        try:
+            # Truy vấn CSDL để lấy ID_user
+            query_user_id = 'SELECT ID_user FROM User_Accounts WHERE Username = ?'
+            cursor.execute(query_user_id, (author,))
+            row = cursor.fetchone()
+            if row is None:
+                    return 'Người dùng không tồn tại.'
+            
+            user_id = row[0]
+
+            query_update = "UPDATE Articles SET Title=?, Content=?, Descript=? WHERE ID_article=?"
+            cursor.execute(query_update, (title, content, description, article_id))
+            conn.commit()
+            
+            flash('Chỉnh sửa bài viết thành công!', 'success')
+
+            return redirect(url_for('member', ID_User=user_id))
+        
+        except pyodbc.Error as e:
+            flash(f'Lỗi khi sửa bài viết: {str(e)}', 'danger')
+    
+    return render_template('member.html')
+
+@app.route('/edit-account', methods=['GET'])
+@login_required
+def edit_account():
+    return render_template('AccountManager.html')
+
+@app.route('/update-account', methods=['POST'])
+@login_required
+def update_account():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+
+    query_update = "UPDATE User_Accounts SET Username = ?, Email = ?, Pass_word = ? WHERE ID_user = ?"
+    cursor.execute(query_update,(username, email, password, current_user.id))
+    conn.commit()
+
+    flash('Account updated successfully!', 'success')
+    return redirect(url_for('member'))
+
+@app.route('/delete-account', methods=['POST'])
+@login_required
+def delete_account():
+    print(current_user.id)
+    query_delete_content = "DELETE FROM Articles WHERE ID_user = ?"
+    cursor.execute(query_delete_content,(current_user.id))
+
+    query_delete_accont = "DELETE FROM User_Accounts WHERE ID_user = ?"
+    cursor.execute(query_delete_accont,(current_user.id))
+    conn.commit()
+
+    logout_user()
+
+    flash('Account deleted successfully!', 'success')
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
